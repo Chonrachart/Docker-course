3bdbfc97a0f60f6e057b88c6304c5f85110777c2
Chonrachart <chonrachart.sieng@gmail.com>
2026-03-22 17:53:34 +0700

    update bind mount

--- 7-containerize-react-app/src/App.tsx
+++ 7-containerize-react-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello form bule nginx
+          Hello form a brand new nginx
         </p>
         <a
           className="App-link"
